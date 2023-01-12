cf5246b9da919f1044029c389d328b64b4ed9f62
korrawee <korrawee.somyos@gmail.com>
2023-01-13 03:31:27 +0700

    Add : url api

---
+++ src/utils/api/url.ts
@@ -0,0 +1,17 @@
+interface craeteUrlRes {
+    shortUrl: string;
+}
+
+
+export async function createUrl(url: string): Promise<string> {
+    const res: Response = await fetch('http://shorturl/', {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({originalUrl: url})
+    });
+    const content: craeteUrlRes = await res.json()
+    return content.shortUrl;
+}
